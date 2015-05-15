89620428865c05858560ecf398483e4f8b3575d9
a young adelina Lee <lay126@gmail.com>
2015-05-16 03:46:43 +0900

    mouse over

--- public/js/sgen.js
+++ public/js/sgen.js
@@ -1,3 +1,40 @@
-$( document ).ready( function(){	
-
+$(document).ready(function(){
+    $("#title_home").mouseover(function(){
+        $("#title_home").css("background-color", "#002654");
+    });
+    $("#title_home").mouseout(function(){
+        $("#title_home").css("background-color", "#0098d8");
+    });
+});
+$(document).ready(function(){
+    $("#title_mail").mouseover(function(){
+        $("#title_mail").css("background-color", "#002654");
+    });
+    $("#title_mail").mouseout(function(){
+        $("#title_mail").css("background-color", "#0098d8");
+    });
+});
+$(document).ready(function(){
+    $("#title_calender").mouseover(function(){
+        $("#title_calender").css("background-color", "#002654");
+    });
+    $("#title_calender").mouseout(function(){
+        $("#title_calender").css("background-color", "#0098d8");
+    });
+});
+$(document).ready(function(){
+    $("#title_me").mouseover(function(){
+        $("#title_me").css("background-color", "#002654");
+    });
+    $("#title_me").mouseout(function(){
+        $("#title_me").css("background-color", "#0098d8");
+    });
+});
+$(document).ready(function(){
+    $("#title_setting").mouseover(function(){
+        $("#title_setting").css("background-color", "#002654");
+    });
+    $("#title_setting").mouseout(function(){
+        $("#title_setting").css("background-color", "#0098d8");
+    });
 });
